12df32d087825bfbf2f69409590ceb14fa1c7bc0
Joseph Neiva <neivajoseph@gmail.com>
2023-10-17 20:09:20 -0300

    ++

---
+++ Add_Ons/AgrupamentoML-1.py
@@ -0,0 +1,39 @@
+import numpy as np
+from sklearn import metrics
+from sklearn.datasets import make_blobs
+from sklearn.preprocessing import StandardScaler
+import hdbscan
+import matplotlib.pyplot as plt
+
+# Generate sample data
+centers = [[1, 1], [-1, -1], [1, -1]]
+X, labels_true = make_blobs(n_samples=750, centers=centers, cluster_std=0.4,
+                            random_state=0)
+
+X = StandardScaler().fit_transform(X)
+
+# Compute HDBSCAN
+clusterer = hdbscan.HDBSCAN(min_samples=10, gen_min_span_tree=True)
+labels = clusterer.fit_predict(X)
+
+# Number of clusters in labels, ignoring noise if present.
+n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+n_noise_ = list(labels).count(-1)
+
+# Plot result
+unique_labels = set(labels)
+colors = [plt.cm.Spectral(each)
+          for each in np.linspace(0, 1, len(unique_labels))]
+for k, col in zip(unique_labels, colors):
+    if k == -1:
+        # Black used for noise.
+        col = [0, 0, 0, 1]
+
+    class_member_mask = (labels == k)
+
+    xy = X[class_member_mask]
+    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+             markeredgecolor='k', markersize=6)
+
+plt.title('Estimated number of clusters: %d' % n_clusters_)
+plt.show()
